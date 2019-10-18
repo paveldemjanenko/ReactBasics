eb33573a11859df686f2248ecaac23493baaf788
Demjanenko <pavels.demjanenko@accenture.com>
2019-10-18 10:39:47 +0300

    Updates

---
+++ precache-manifest.c6c4de473bf5c662e0b815ed4310338c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6bb6896728f2673933e30e7797d9b8",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2e92b8d7f50a68825f75",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "8d785cd6ae7d5b3ab500",
+    "url": "/react-tutorial/static/js/2.afaabb5f.chunk.js"
+  },
+  {
+    "revision": "2e92b8d7f50a68825f75",
+    "url": "/react-tutorial/static/js/main.a207c7ab.chunk.js"
+  },
+  {
+    "revision": "acd1605a6af8f99b39b9",
+    "url": "/react-tutorial/static/js/runtime-main.b18dd318.js"
+  }
+]);
